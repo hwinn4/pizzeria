7d7628fb1f28ab170636246f03524f646f5e00e8
Hilary Winn <hilary@fuigo.com>
2019-02-25 20:09:58 -0500

    tests order#total_price
    This was already tested via an integration test, but I have now added the unit test as well to provide additional examples of the functionality.

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -3,9 +3,31 @@
 require 'rails_helper'
 
 RSpec.describe Order do
-  subject { Order.new }
+  subject { FactoryBot.create(:order) }
 
   specify 'associations' do
     should have_many :order_items
+  end
+
+  describe '#total_price' do
+    context 'when the order is not empty' do
+      let(:pizza_with_cents1) { FactoryBot.create(:pizza, price: Money.new(1733)) }
+      let(:pizza_with_cents2) { FactoryBot.create(:pizza, price: Money.new(1121)) }
+
+      it 'returns the sum of all related order item prices' do
+        order_item1 = FactoryBot.create(:order_item, pizza_id: pizza_with_cents1.id, quantity: 2)
+        order_item2 = FactoryBot.create(:order_item, pizza_id: pizza_with_cents2.id, quantity: 5)
+        subject.order_items << order_item1
+        subject.order_items << order_item2
+
+        expect(subject.total_price).to eq(Money.new(9071))
+      end
+    end
+
+    context 'when the order is empty' do
+      it 'returns 0' do
+        expect(subject.total_price).to eq(Money.new(0))
+      end
+    end
   end
 end
